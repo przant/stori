4068e67e57596e582f5582bfc5bcb0118cb41652
Antonio Pérez <apema2079@gmail.com>
2023-04-18 06:12:56 -0600

    Add Golang local client CLI APP

---
+++ file_uploader/go.mod
@@ -0,0 +1,7 @@
+module github.com/przant/stori/file_uploader
+
+go 1.19
+
+require github.com/aws/aws-sdk-go v1.44.245
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
---
+++ file_uploader/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+)
+
+var (
+	bucket   string
+	filename string
+)
+
+func init() {
+	flag.Usage = func() {
+		fmt.Printf("Usage: %s -f <filename> -b <bucket_name>\n\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+}
+
+func main() {
+	flag.StringVar(&bucket, "b", "", "S3 bucket name in where you want to upload the file")
+	flag.StringVar(&filename, "f", "", "File to upload in the bucket given for the flag -b")
+
+	flag.Parse()
+
+	if bucket == "" || filename == "" {
+		log.Printf("You have to indicate the file to upload and the bucket destination!\n\n")
+		flag.Usage()
+	}
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("Error trying to open the file %q: %s\n", filename, err)
+	}
+
+	defer file.Close()
+
+	sess, err := session.NewSession(
+		&aws.Config{
+			Region: aws.String("us-east-1"),
+		},
+	)
+	if err != nil {
+		log.Fatalf("Error trying to creating a new AWS Session: %s\n", err)
+	}
+
+	uploader := s3manager.NewUploader(sess)
+
+	filename = fmt.Sprintf("%s_%d.csv", strings.TrimSuffix(filename, ".csv"), time.Now().Unix())
+
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(filename),
+		Body:   file,
+	})
+
+	if err != nil {
+		log.Fatalf("Unable to upload %q to %q: %s\n\n", filename, bucket, err)
+	} else {
+		fmt.Printf("Successfully uploaded %q to %q\n", filename, bucket)
+	}
+}
